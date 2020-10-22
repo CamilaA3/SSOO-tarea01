b93034b818313f5fbdb41d48e34e41cb58919b56
CamilaA3 <70764096+CamilaA3@users.noreply.github.com>
2020-10-22 20:46:39 -0300

    Update hora.c

--- src/hora.c
+++ src/hora.c
@@ -4,14 +4,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+//Contador para la interrupcion "CTRL+C". Se crea fuera de la funcion salida() ya que si se crea dentro, se reinicia la cuenta y no avanza.
+int conta=0;
+
+//Calcula las veces que se ingresa la interrupcion "CTRL+C". Si llega a 2, el proceso termina.
 void salida(int signum){
-	int conta=0;
 	conta++;
 	if(conta==2){
 		exit(signum);
 	}
 }
-
+//Establece el tiempo segun los requirimientos para la tarea.
 void tiempo(){
     char final[100];//Cadena de caracteres que nos indicara el tiempo al final de la funcion.
     time_t tiempo= time(0);//Se crea la variable y la definimos vacia para evitar problemas futuros.
@@ -21,8 +24,8 @@ void tiempo(){
 }
 
 //Verifica el PID de la señal
-void signal_handler (int signumero)
-{
+void signal_handler (int signumero){
+	//Verifica si es una señal SIGUSR1
 	if (signumero == SIGUSR1){
 		printf("Senal SIGUSR1 recibida: ");
 		tiempo();
@@ -30,14 +33,15 @@ void signal_handler (int signumero)
 }
 
 int main(int argc, char* argv[]){
-	int contad=0;
+	int contad=0;//Variable para el ciclo infinito.
 	printf("Proceso hora ejecutandose. PID=%d \n",getpid());
 	signal(SIGUSR1, signal_handler);
 	signal(SIGINT, salida);
+	//Ignora las señales SIGUSR2 y la SIGTERM
 	signal(SIGUSR2,SIG_IGN);
 	signal(SIGTERM,SIG_IGN);
 	while(contad==0){
-		sleep(2);
+		sleep(2);//Duerme por dos segundos para que se pueda apreciar lo que hace el proceso.
 		printf("Listo para recibir la senal SIGUSR1.\n");
 	}
 	return 0;
